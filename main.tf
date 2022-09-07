7ed52bcfff705fe7a62e27af34910d2a37845152
Lam Tu <lam.tu@xero.com>
2022-09-07 13:10:00 +1000

    Specify default tag

--- main.tf
+++ main.tf
@@ -11,6 +11,16 @@ terraform {
 
 provider "aws" {
   region = "ap-southeast-2"
+
+ // this only gets added to resources created by terraform. 
+ // if there are dynamic resources (e.g EC2 instances created by Autoscaling groups), the new resources won't get tagged
+ // follow https://learn.hashicorp.com/tutorials/terraform/aws-default-tags?in=terraform/aws#propagate-default-tags-to-auto-scaling-group to mitigate this
+
+  default_tags {     
+    tags = {
+      uuid = "BvZPganedGfYklJIvCIFX4"
+    }
+  }
 }
 
 resource "aws_instance" "app_server" {
@@ -20,6 +30,5 @@ resource "aws_instance" "app_server" {
 
   tags = {
     Name = var.instance_name
-    uuid = "BvZPganedGfYklJIvCIFX4"
   }
 }
